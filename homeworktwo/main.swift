de2ba1c9fd1db1fffbfded92492d8c5dd1be71fd
islam <islamryskeldiev@icloud.com>
2022-09-15 15:35:56 +0600

    homework

--- homeworktwo/main.swift
+++ homeworktwo/main.swift
@@ -1,11 +1,92 @@
-//
-//  main.swift
-//  homeworktwo
-//
-//  Created by Mac on 15/9/22.
-//
+
 
 import Foundation
 
-print("Hello, World!")
 
+
+import Foundation
+
+func square(number: Int) {
+    print(number * number)
+}
+square(number: 6)
+
+func cube(number:Int) {
+    print(number * number * number)
+}
+cube(number: 8)
+func perimetr(number1:Int,number2:Int,number3:Int,number4: Int) {
+    print(number1 +  number2 + number3 + number4)
+}
+perimetr(number1: 5, number2: 7, number3: 8, number4: 4)
+
+func ploshad(number1: Int,number2:Int){
+print((number1 + number2) * 2)
+}
+ploshad(number1: 5, number2: 7)
+
+func treugolnik(number1:Double, number2:Double,number3:Double) {
+    print(number1 * (number2 * number3))
+}
+treugolnik(number1: 3.14, number2: 4.0, number3: 4.0)
+
+    
+
+
+func plus(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+print(plus(num1: 5, num2: 10))
+print(plus(num1: 2, num2: 4))
+print(plus(num1: 6, num2: 9))
+print(plus(num1: 2, num2: 8))
+print(plus(num1: 5, num2: 4))
+print(plus(num1: 5, num2: 6))
+print(plus(num1: 8, num2: 3))
+print(plus(num1: 9, num2: 3))
+print(plus(num1: 12, num2: 5))
+print(plus(num1: 11, num2: 2))
+
+func minus(num1: Int, num2: Int) -> Int {
+    return num1 + num2
+}
+print(minus(num1: 10, num2: 5))
+print(minus(num1: 14, num2: 2))
+print(minus(num1: 1, num2: 12))
+print(minus(num1: 7, num2: 2))
+print(minus(num1: 5, num2: 3))
+print(minus(num1: 13, num2: 7))
+print(minus(num1: 9, num2: 2))
+print(minus(num1: 2, num2: 6))
+print(minus(num1: 13, num2: 2))
+print(minus(num1: 2, num2: 12))
+
+
+func divide(num1: Int,num2: Int) -> Int {
+    return num1 / num2
+}
+print(divide(num1: 15, num2:3))
+print(divide(num1: 26, num2:13))
+print(divide(num1: 15, num2:5))
+print(divide(num1: 18, num2:6))
+print(divide(num1: 18, num2:9))
+print(divide(num1: 8, num2:4))
+print(divide(num1: 15, num2:3))
+print(divide(num1: 14, num2:2))
+print(divide(num1: 21, num2:7))
+print(divide(num1: 16, num2:8))
+
+
+func multiply(num1:Int,num2: Int)-> Int {
+    return num1 * num2
+}
+print(multiply(num1: 5, num2: 5))
+print(multiply(num1: 10, num2: 5))
+print(multiply(num1: 12, num2: 3))
+print(multiply(num1: 6, num2: 7))
+print(multiply(num1: 12, num2: 8))
+print(multiply(num1: 16, num2: 5))
+print(multiply(num1: 5, num2: 14))
+print(multiply(num1: 7, num2: 5))
+print(multiply(num1: 23, num2: 2))
+print(multiply(num1: 25, num2: 4))
